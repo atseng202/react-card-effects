18c671c7646edee319ddb4d303d299123beea8df
Winnie Chou <winnie.chou.mb@gmail.com>
2021-01-22 17:18:00 -0600

    code review comments

--- src/Deck.js
+++ src/Deck.js
@@ -11,8 +11,8 @@ const BASE_URL = "https://deckofcardsapi.com/api/deck";
  * Deck renders a container a draw button and list of Cards
  * 
  * props: deckCount: int, number of decks, default 1
- * 
- * state: cards, array like:
+ * TODO:
+ * state: isLoading, deck, cards, array like:
  *  [{
       "image": "https://deckofcardsapi.com/static/img/KH.png",
       "value": "KING",
@@ -20,15 +20,16 @@ const BASE_URL = "https://deckofcardsapi.com/api/deck";
       "code": "KH"
     }, ....]
  * 
- * evt: listens for click on draw button
  * 
  * App -> Deck -> Card
  **/
 
 function Deck() {
+  // TODO: Be more specific with loading - what are you loading
   const [isLoading, setIsLoading] = useState(true);
   const [deck, setDeck] = useState(null);
   const [cards, setCards] = useState([]);
+  // TODO: from should to is/has/etc
   const [shouldDraw, setShouldDraw] = useState(false);
   console.debug("rendering deck= ", deck);
   console.debug("rendering cards= ", cards);
@@ -40,6 +41,7 @@ function Deck() {
   useEffect(function fetchNewDeckOnMount() {
 
     async function fetchNewDeck() {
+      // TODO: add error alert with try/catch
       console.debug("fetchNewDeck");
       const deckResult = await axios.get(`${BASE_URL}/new/`);
       setDeck(deckResult.data);
@@ -47,15 +49,18 @@ function Deck() {
     }
 
     fetchNewDeck();
+    // Can include setDeck and isLoading as dependencies
   }, []);
 
+  // TODO: Change function name, not actually on click
   /** Fetches a new card when shouldDraw is true */
   useEffect(function fetchNewCardOnClick() {
-
+    // TODO: include isLoading as API scales
     async function fetchNewCard() {
       console.debug("fetchNewCard: deck=", deck);
       const cardResult = await axios.get(`${BASE_URL}/${deck.deck_id}/draw/?count=1`);
-
+      // TODO: can use a try/catch and handle this error and/or API error
+      // TODO: use explicit piece of data from API
       if (cardResult.data.error) {
         alert("Error: no cards remaining!");
       } else {
@@ -64,7 +69,7 @@ function Deck() {
             ...prevCards,
             {
               ...cardResult.data.cards[0],
-              id: uuid()
+              id: uuid() // TODO: use code from card data
             }
           ]
         });
@@ -82,14 +87,12 @@ function Deck() {
     setShouldDraw(true);
   }
 
-  let renderCards = (
-    cards.map(card => (
+  let renderCards = cards.map(card => (
       <Card
-        key={card.id}
+        key={card.code}
         card={card}
       />
-    ))
-  );
+    ));
 
   if (isLoading) return <i>Loading...</i>;
   
