21bbde00d7f33b028d3959ce5c2d0c75e879c6a3
Winnie Chou <winnie.chou.mb@gmail.com>
2021-01-22 18:02:46 -0600

    code review comments

--- src/Deck.js
+++ src/Deck.js
@@ -1,39 +1,50 @@
 import { useState, useEffect } from "react";
 import Card from "./Card";
 import axios from "axios";
-import { v4 as uuid } from 'uuid';
 
-// const NEW_DECK_URL = "https://deckofcardsapi.com/api/deck/new";
-// /<<deck_id>>/draw/?count=2";
 const BASE_URL = "https://deckofcardsapi.com/api/deck";
 
 /** 
  * Deck renders a container a draw button and list of Cards
  * 
- * props: deckCount: int, number of decks, default 1
- * TODO:
- * state: isLoading, deck, cards, array like:
+ * props: none
+ * 
+ * state: 
+ * - isLoading
+ * - deck, object like:
+ * {
+    "success": true,
+    "deck_id": "3p40paa87x90",
+    "shuffled": false,
+    "remaining": 52
+    }
+ * - cards, array like:
  *  [{
       "image": "https://deckofcardsapi.com/static/img/KH.png",
       "value": "KING",
       "suit": "HEARTS",
       "code": "KH"
     }, ....]
- * 
+ * - isDrawing: Boolean
+ * - isShuffling: Boolean
+ * - error: TODO:
  * 
  * App -> Deck -> Card
  **/
 
 function Deck() {
-  // TODO: Be more specific with loading - what are you loading
+  // Loading for getting a new deck, drawing a new card, for shuffling
   const [isLoading, setIsLoading] = useState(true);
   const [deck, setDeck] = useState(null);
   const [cards, setCards] = useState([]);
-  // TODO: from should to is/has/etc
-  const [shouldDraw, setShouldDraw] = useState(false);
+  const [isDrawing, setIsDrawing] = useState(false);
+  const [isShuffling, setIsShuffling] = useState(false);
+  const [error, setError] = useState(null);
+  console.debug("rendering isLoading= ", isLoading);
   console.debug("rendering deck= ", deck);
   console.debug("rendering cards= ", cards);
-  console.debug("rendering shouldDraw= ", shouldDraw);
+  console.debug("rendering isDrawing= ", isDrawing);
+  console.debug("rendering isShuffling= ", isShuffling);
 
   /** Fetch a new deck id and meta info about deck 
    * when component renders the first time 
@@ -41,50 +52,88 @@ function Deck() {
   useEffect(function fetchNewDeckOnMount() {
 
     async function fetchNewDeck() {
-      // TODO: add error alert with try/catch
       console.debug("fetchNewDeck");
-      const deckResult = await axios.get(`${BASE_URL}/new/`);
-      setDeck(deckResult.data);
-      setIsLoading(false);
+      let deckResult;
+      try {
+      deckResult = await axios.get(`${BASE_URL}/new/`);
+      } catch (err){
+        setError(err.message);
+      } finally {
+        setDeck(deckResult.data);
+        setIsLoading(false);
+      }
     }
 
     fetchNewDeck();
-    // Can include setDeck and isLoading as dependencies
   }, []);
 
-  // TODO: Change function name, not actually on click
-  /** Fetches a new card when shouldDraw is true */
-  useEffect(function fetchNewCardOnClick() {
-    // TODO: include isLoading as API scales
-    async function fetchNewCard() {
+  /** Fetches a new card when isDrawing is true */
+  useEffect(function fetchNewCard() {
+
+    async function _fetchNewCard() {
       console.debug("fetchNewCard: deck=", deck);
-      const cardResult = await axios.get(`${BASE_URL}/${deck.deck_id}/draw/?count=1`);
-      // TODO: can use a try/catch and handle this error and/or API error
-      // TODO: use explicit piece of data from API
-      if (cardResult.data.error) {
-        alert("Error: no cards remaining!");
-      } else {
-        setCards(prevCards => {
-          return [
-            ...prevCards,
-            {
-              ...cardResult.data.cards[0],
-              id: uuid() // TODO: use code from card data
-            }
-          ]
-        });
+      let cardResult;
+      try {
+        cardResult = await axios.get(`${BASE_URL}/${deck.deck_id}/draw/?count=1`);
+      } catch (err){
+        setError(err.message);
+      } finally {
+        // TODO: Fix this 
+        if (cardResult.data.remaining === 0) {
+          alert("Error: no cards remaining!");
+        } else {
+          setCards(prevCards => {
+            return [
+              ...prevCards,
+              {...cardResult.data.cards[0]}
+            ]
+          });
+        }
+        console.debug("fetchNewCard after axios deck= ", deck);
+        // prevent drawing on next re-render
+        setIsDrawing(false);
+        setIsLoading(false);
       }
-      console.debug("fetchNewCard after axios deck= ", deck);
-      // prevent drawing on next re-render
-      setShouldDraw(false);
     }
 
-    if (shouldDraw) fetchNewCard();
-  }, [shouldDraw]);
+    if (isDrawing) _fetchNewCard();
+  }, [isDrawing]);
+
+  /** Shuffles deck when shuffle is true */
+  useEffect(function shuffleDeck() {
+    async function _shuffleDeck() {
+      console.debug("shuffleDeck: deck=", deck);
+      let deckResult;
+      try {
+        deckResult = await axios.get(`${BASE_URL}/${deck.deck_id}/shuffle/`);
+        setDeck(deckResult.data);
+      } catch (err){
+        setError(err.message);
+      } finally {
+        setCards([]);
+
+        console.debug("fetchNewCard after axios deck= ", deck);
+        // prevent shuffling on next re-render
+        setIsShuffling(false);
+        setIsLoading(false);
+      }
+    }
+
+    if (isShuffling) _shuffleDeck();
+  }, [isShuffling]);
 
   /** Draw a card from cards in state */
   function handleDrawCard(evt) {
-    setShouldDraw(true);
+    // Fail fast if shuffle is true do not draw card
+    setIsLoading(true);
+    if (isShuffling) return;
+    setIsDrawing(true);
+  }
+
+  /** Draw a card from cards in state */
+  function handleShuffle(evt) {
+    setIsLoading(true);
+    setIsShuffling(true);
   }
 
   let renderCards = cards.map(card => (
@@ -93,12 +142,17 @@ function Deck() {
         card={card}
       />
     ));
+  
+  // TODO: resetting error with successful API path
+  let errorMsg = error && <b>Oh no! {error}</b>;
 
   if (isLoading) return <i>Loading...</i>;
   
   return (
     <div className="Deck">
       <button onClick={handleDrawCard}>Gimme a card!</button>
+      <button onClick={handleShuffle}>Shuffle!</button>
+      {errorMsg}
       <div>{renderCards}</div>
     </div>
   )
